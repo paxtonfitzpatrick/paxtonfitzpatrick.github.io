b83f86a3384cffc35492222fb1c525dcdec1ffc8
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-06-20 17:29:13 -0400

    bug fixes

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -1,8 +1,7 @@
 const TimelineDisplayer = function(tag_id, timeline_el, canvas) {
   this.timelineObj = {
     canvas: {
-      el: canvas,
-      ctx: canvas.getContext('2d')
+      el: canvas
     },
     // get start and end years from element dataset
     start_year: parseInt(timeline_el.dataset.start),
@@ -25,9 +24,11 @@ const TimelineDisplayer = function(tag_id, timeline_el, canvas) {
   ========================================
   */
   TL.functions.timeline.init = function() {
+    TL.canvas.ctx = TL.canvas.el.getContext('2d');
+    TL.functions.events.parseEvents();
     // also need to load images and wait before moving on
     // compute y-coordinate for each 1/4 year included in the timeline
-    const y_inc = TL.canvas.height / ((TL.end_year - TL.start_year) * 4);
+    const y_inc = TL.canvas.el.height / ((TL.end_year - TL.start_year) * 4);
     for (let year = TL.start_year, year_y = 0; year <= TL.end_year; year += 0.25, year_y += y_inc) {
       TL.years_ycoords[year] = Math.floor(year_y);
       TL.occupied_grid[year] = Array(TL.events.length).fill(0);
@@ -37,12 +38,12 @@ const TimelineDisplayer = function(tag_id, timeline_el, canvas) {
   TL.functions.timeline.getStyle = function() {
     // styling set via sass variables and stored as custom properties in timeline element
     const compStyles = getComputedStyle(timeline_el);
-    TL.style.background_color = compStyles.getPropertyValue("--background-color");
-    TL.style.year_color = compStyles.getPropertyValue("--year-color");
-    TL.style.year_fontsize = compStyles.getPropertyValue("--year-font-size");
-    TL.style.gridline_color = compStyles.getPropertyValue("--gridline-color");
+    TL.style.background_color = compStyles.getPropertyValue("--background-color").trim();
+    TL.style.year_color = compStyles.getPropertyValue("--year-color").trim();
+    TL.style.year_fontsize = compStyles.getPropertyValue("--year-font-size").trim();
+    TL.style.gridline_color = compStyles.getPropertyValue("--gridline-color").trim();
     TL.style.gridline_width = parseInt(compStyles.getPropertyValue("--gridline-width"));
-    TL.style.timeline_color = compStyles.getPropertyValue("--timeline-color");
+    TL.style.timeline_color = compStyles.getPropertyValue("--timeline-color").trim();
     TL.style.timeline_width = parseInt(compStyles.getPropertyValue("--timeline-width"));
     TL.style.event_width = parseInt(compStyles.getPropertyValue("--event-width"));
     TL.style.event_offset = parseInt(compStyles.getPropertyValue("--event-offset"));
@@ -79,6 +80,7 @@ const TimelineDisplayer = function(tag_id, timeline_el, canvas) {
 
   TL.functions.timeline.drawBase = function() {
     // draw main vertical timeline
+    TL.canvas.ctx.clearRect(0, 0, TL.canvas.el.width, TL.canvas.el.height);
     TL.canvas.ctx.beginPath();
     TL.canvas.ctx.moveTo(Math.round(TL.canvas.width / 2), 0);
     TL.canvas.ctx.lineTo(Math.round(TL.canvas.width / 2), TL.canvas.height);
@@ -112,10 +114,10 @@ const TimelineDisplayer = function(tag_id, timeline_el, canvas) {
   */
   TL.functions.events.TimelineEvent = function(event_li) {
     this.title = event_li.id.replace("timeline-event", "").replace("_", " ");
-    this.start_year = parseFloat(event_li.dataset.start).toFixed(2);
-    this.end_year = parseFloat(event_li.dataset.end).toFixed(2);
-    this.color = event_li.dataset.color;
-    this.image_path = event_li.dataset.image;
+    this.start_year = parseFloat(event_li.dataset.start);
+    this.end_year = parseFloat(event_li.dataset.end);
+    this.color = `#${event_li.dataset.color}`;
+    // this.image_path = event_li.dataset.image;
   };
 
   TL.functions.events.TimelineEvent.prototype.draw = function() {
@@ -144,20 +146,14 @@ const TimelineDisplayer = function(tag_id, timeline_el, canvas) {
   TL.functions.launch = function() {
     TL.functions.timeline.init();
     TL.functions.timeline.getStyle();
-    TL.functions.events.parseEvents();
-    Tl.functions.timeline.computeEventLayout();
+    TL.functions.timeline.computeEventLayout();
     TL.functions.timeline.drawBase();
     for (let event of TL.events) {
       event.draw();
     }
-  }
-
+  };
   TL.functions.launch();
 };
-
-
-// const bg_color = getComputedStyle(timeline_el).getPropertyValue("--background-color");
-// console.log(bg_color);
 
 /*
 ========================================
@@ -180,5 +176,3 @@ window.timelineDisplay = function(tag_id) {
     timelineDom.push(new TimelineDisplayer(tag_id, timeline_el, canvas));
   }
 };
-
-timelineDisplay('timeline');
