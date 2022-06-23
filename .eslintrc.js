ad0e838fed52eaf68bec59cd22e9564e28453bae
paxtonfitzpatrick <Paxton.C.Fitzpatrick@Dartmouth.edu>
2022-06-23 04:33:40 -0400

    set sourceType to script

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,14 @@
 /* eslint-env node */
 module.exports = {
   extends: ['airbnb-base', 'plugin:compat/recommended'],
+  plugins: ['compat'],
   env: {
     browser: true,
     es6: true,
     jquery: true,
     node: false,
+  },
+  parserOptions: {
+    sourceType: 'script',
   },
 };
