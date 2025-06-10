6a43ecea412b089d7900137cb746923151d2e71d
paxtonfitzpatrick <Paxton.C.Fitzpatrick@Dartmouth.edu>
2025-06-10 19:09:15 -0400

    update eslint, use new config format, replace deprecated packages & plugins

---
+++ eslint.config.js
@@ -0,0 +1,59 @@
+import path from 'node:path';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import { includeIgnoreFile } from '@eslint/compat';
+import js from '@eslint/js';
+import compat from 'eslint-plugin-compat';
+import { configs, plugins } from 'eslint-config-airbnb-extended';
+import globals from 'globals';
+
+export default defineConfig([
+  // inherit ignores from .gitignore, plus minified files & source maps.
+  // (**/node_modules/ & .git/ are ignored by default)
+  // must be outside main config block to work because .gitignore includes dir
+  // patterns, and non-global ignores can only match file names
+  globalIgnores([
+    ...includeIgnoreFile(path.resolve('.gitignore')).ignores,
+    '**/*.min.js',
+    '**/*.min.js.map',
+  ]),
+  // main config
+  {
+    files: ['**/*.js'],
+    plugins: {
+      js,
+      // plugins required internally by eslint-config-airbnb-base
+      '@stylistic': plugins.stylistic.plugins['@stylistic'],
+      'import-x': plugins.importX.plugins['import-x'],
+    },
+    extends: [
+      'js/recommended',
+      configs.base.recommended,
+      compat.configs['flat/recommended'],
+    ],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'script',
+      globals: { ...globals.browser, ...globals.jquery },
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: 'warn',
+      reportUnusedInlineConfigs: 'warn',
+    },
+    rules: {
+      'comma-dangle': ['error', 'always-multiline'],
+      'no-empty': ['error', { allowEmptyCatch: true }],
+      'no-plusplus': 'off',
+      'object-curly-newline': ['error', { multiline: true, consistent: true }],
+      'one-var': ['error', 'consecutive'],
+      'operator-linebreak': ['error', 'before'],
+      strict: ['error', 'global'],
+    },
+  },
+  // consider this file a module so imports/exports don't prevent linting
+  {
+    files: ['eslint.config.js'],
+    languageOptions: {
+      sourceType: 'module',
+    },
+  },
+]);
