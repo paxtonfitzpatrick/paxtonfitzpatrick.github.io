b4ac72044b7821417ef3839cd2ab77937c114df4
paxtonfitzpatrick <Paxton.C.Fitzpatrick@Dartmouth.edu>
2025-07-04 22:22:03 -0400

    optimize scroll shadow performance

--- assets/js/scroll-shadows.js
+++ assets/js/scroll-shadows.js
@@ -1,44 +1,50 @@
 'use strict';
 
 (() => {
-  // check whether browser supports passive eventListeners
-  let passiveOptsOrUseCapture = false;
-  try {
-    const opts = Object.defineProperty({}, 'passive', {
-      // eslint-disable-next-line getter-return
-      get: () => { passiveOptsOrUseCapture = { passive: true }; },
-    });
-    window.addEventListener('checkPEL', null, opts);
-    window.removeEventListener('checkPEL', null, opts);
-  } catch {}
+  function initScrollShadows() {
+    // detect passive event listener support
+    let passiveOptsOrUseCapture = false;
+    try {
+      const opts = Object.defineProperty({}, 'passive', {
+        // eslint-disable-next-line getter-return
+        get: () => { passiveOptsOrUseCapture = { passive: true }; },
+      });
+      window.addEventListener('testPassive', null, opts);
+      window.removeEventListener('testPassive', null, opts);
+    } catch {}
 
-  // get elements with "scroll-shadows" class
-  const scrollShadowElements = document.getElementsByClassName('scroll-shadows');
+    const scrollShadowElements = document.getElementsByClassName('scroll-shadows');
 
-  // initialize scroll shadows
-  Array.prototype.forEach.call(scrollShadowElements, (element) => {
-    // div starts out unscrolled, so add bottom shadow
-    element.classList.add('scroll-shadow-bottom');
-    // attach eventListeners to add/remove shadows on scroll
-    element.addEventListener('scroll', () => {
-      // destructuring
-      const { classList, offsetHeight, scrollHeight, scrollTop } = element;
+    Array.prototype.forEach.call(scrollShadowElements, (element) => {
+      // set initial state of shadows
+      updateShadows(element);
 
-      if (scrollTop > 0) {
-        // if element is scrolled, add top shadow
-        classList.add('scroll-shadow-top');
-        if (scrollTop < scrollHeight - offsetHeight) {
-          // if element is not scrolled all the way to the bottom, also add
-          // bottom shadow
-          classList.add('scroll-shadow-bottom');
-        } else {
-          // if scrolled to bottom, remove bottom shadow (if present)
-          classList.remove('scroll-shadow-bottom');
+      // optimize scroll performance
+      let ticking = false;
+      element.addEventListener('scroll', () => {
+        if (!ticking) {
+          requestAnimationFrame(() => {
+            updateShadows(element);
+            ticking = false;
+          });
+          ticking = true;
         }
-      } else {
-        // if element is not scrolled, remove top shadow (if present)
-        classList.remove('scroll-shadow-top');
-      }
-    }, passiveOptsOrUseCapture);
-  });
+      }, passiveOptsOrUseCapture);
+    });
+  }
+
+  function updateShadows(element) {
+    const { classList, offsetHeight, scrollHeight, scrollTop } = element,
+          atTop = scrollTop <= 0,
+          atBottom = scrollTop >= scrollHeight - offsetHeight;
+
+    classList.toggle('scroll-shadow-top', !atTop);
+    classList.toggle('scroll-shadow-bottom', !atBottom);
+  }
+
+  if (document.readyState !== 'loading') {
+    initScrollShadows();
+  } else {
+    document.addEventListener('DOMContentLoaded', initScrollShadows);
+  }
 })();
